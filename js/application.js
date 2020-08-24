0b0a9a833a8310e2d7a796a9b6e3785e5c59ab31
Atharva-Kulkarni606 <65156793+Atharva-Kulkarni606@users.noreply.github.com>
2020-08-24 09:11:24 -0700

    Commit 1

---
+++ js/application.js
@@ -0,0 +1,109 @@
+let btn = document.getElementById('btn1');
+showNote();
+btn.addEventListener('click', AddNote);
+
+
+function AddNote() {
+    let inputText = document.getElementById('note1');
+
+
+    let inputTitle=document.getElementById('title-ip');
+
+
+    notes = localStorage.getItem('notes');
+
+    let inputObj={
+        title: inputTitle.value,
+        text :inputText.value
+    }
+
+    if (notes == null) {
+        ArrayObj = []
+    } else {
+        ArrayObj = JSON.parse(notes);
+    }
+    let blankIndex= ArrayObj.indexOf("");
+   
+
+    ArrayObj.push(inputObj);
+    if(ArrayObj.includes("")){
+        ArrayObj.splice(blankIndex,1);
+    }
+    localStorage.setItem('notes', JSON.stringify(ArrayObj));
+    inputText.value = null;
+
+    inputTitle=null;
+
+    showNote();
+}
+
+//Reads data from Local Storage and displays it 
+function showNote() {
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        ArrayObj = [];
+    } else {
+        ArrayObj = JSON.parse(notes);
+    }
+
+
+   
+
+    let string1 = "";
+
+    ArrayObj.forEach(function (element, index) {
+        string1 += `
+        <div class="n-note">
+        <h2 class="n-note-heading">${element.title}</h2>
+        <p class="n-para">${element.text}</p>
+        <button id="${index}" onclick="deleteNode(this.id)" class="n-btn">Delete Node</button>
+    </div>
+        `
+    });
+
+    let Text = document.getElementById('Note');
+    if (ArrayObj.length != 0) {
+        Text.innerHTML = string1;
+    }else{
+        Text.innerHTML=`<h2>Nothing to show.Use Above Section To Add Notes.</h2>`
+    }
+
+}
+
+
+//Function That will delete the node
+function deleteNode(index){
+    notes = localStorage.getItem('notes');
+
+    if (notes == null) {
+        ArrayObj = []
+    } else {
+        ArrayObj = JSON.parse(notes);
+    }
+   ArrayObj.splice(index,1);
+    localStorage.setItem('notes',JSON.stringify(ArrayObj));
+
+   showNote();
+}
+
+let searchText= document.getElementById('s-input');
+searchText.addEventListener('input',function(){
+let variable1=searchText.value.toLowerCase();
+let noteCollection= document.getElementsByClassName('n-note');
+Array.from(noteCollection).forEach(function(element){
+    let variable2= element.getElementsByTagName('p')[0].innerText;
+    if(variable2.includes(variable1)){
+        element.style.display="block";
+    }else{
+        element.style.display="none";
+    }
+});
+});
+
+
+//To clear the  text on the search bar when one clicks on it
+let searchbtn=document.getElementById('search-btn');
+searchbtn.addEventListener('click',function(){
+    let textInSearchBar= document.getElementById('s-input');
+    textInSearchBar.value=null;
+})
